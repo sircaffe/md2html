9bbc923ee204cdfb064189283dc21e878b170570
kprncp <kinanprincipes@gmail.com>
2025-06-11 19:13:28 -0400

    unharcoded input file name

--- main.c
+++ main.c
@@ -27,8 +27,9 @@ void buffer_append_line(Buffer *buffer, char *line) {
 }
 
 // TODO: Fix newline going into buffer lines
-void read_entire_file(Buffer *buffer, char *file_path) {
+bool read_entire_file(Buffer *buffer, char *file_path) {
 	FILE *f = fopen(file_path, "r");
+    if (f == NULL) return false;
 
     size_t lines = 0;
     char cursor;
@@ -42,21 +43,36 @@ void read_entire_file(Buffer *buffer, char *file_path) {
     }
     rewind(f);
     
-    char tmp[255];
+    char tmp[256];
     for (size_t i = 0; i < lines; ++i) {
-        fgets(tmp, 255, f);
+        fgets(tmp, 256, f);
 
         buffer_append_line(buffer, tmp);
     }
 
 	fclose(f);
+    return true;
 }
 
-int main() {
-    Buffer buff = {0};
-    read_entire_file(&buff, "sample.md");
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "ERROR: Invalid number of arguments\n");
+        fprintf(stderr, "Usage: %s <path-to-file>\n", argv[0]);
+        return 1;
+    }
 
-    printf("%s", buff.line[0]);
+    char *file_path = argv[1];
+
+    Buffer buff = {0};
+    if (!read_entire_file(&buff, file_path)) {
+        fprintf(stderr, "ERROR: Could not find file: %s\n", file_path);
+        fprintf(stderr, "Usage: %s <path-to-file>\n", argv[0]);
+        return 1;
+    }
+    
+    for (int i = 0; i < buff.count; ++i) {
+        printf("%s", buff.line[i]);
+    }
     return 0;
 }
 
